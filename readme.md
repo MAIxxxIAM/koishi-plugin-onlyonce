5269bf07830584ac56df77b3bcad67cc9c62791a
MAIxxxIAM <1207108696@qq.com>
2024-01-26 11:35:36 +0800

    first commit

--- readme.md
+++ readme.md
@@ -2,4 +2,14 @@
 
 [![npm](https://img.shields.io/npm/v/koishi-plugin-onlyonce?style=flat-square)](https://www.npmjs.com/package/koishi-plugin-onlyonce)
 
-限制目标发言
+# 📚 描述
+限制群友（广告机器人）每天只能发一条消息的~~口球模拟器~~
+# ✨使用方式
+
+koishi插件市场中搜索onlyonce，并安装，机器人必须是管理权限，之后使用指令 onlyonce @xxx
+会先禁言目标半小时作为提醒（
+
+之后每天该群友就只能说一句话，说完会被禁言至当天23点59分。
+
+## 配置设置
+- 无
--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,48 @@ export const name = 'onlyonce'
 
 export interface Config {}
 
+declare module 'koishi' {
+  export interface Tables {
+    mutelist: Mutelist
+  }
+}
+
+export interface Mutelist{
+  id?:string
+  channelID?:string
+}
+
 export const Config: Schema<Config> = Schema.object({})
 
 export function apply(ctx: Context) {
-  // write your plugin here
+  ctx.model.extend('mutelist', {
+    id: 'string',
+    channelID:'string',
+  }, { primary: 'id' })
+
+  ctx.middleware(async (session, next) => {
+    const { userId, channelId } = session
+    const userID=userId
+    const users = await ctx.database.get('mutelist',{id:userID})
+    if(users.length==0){
+      return next()
+    }
+    if(users[0].channelID==channelId){
+      const nowTime=new Date()
+      const theEnd=new Date(nowTime.getFullYear(),nowTime.getMonth(), nowTime.getDate(), 23, 59, 59, 999)
+      const muteTime=theEnd.getTime()-nowTime.getTime()
+      await ctx.database.upsert('mutelist',[{id:userID,channelID:channelId}])
+      await session.bot.muteGuildMember(channelId,userID,muteTime)
+      return next()
+    }
+    return next()
+  })
+  ctx.command('onlyonce <user>', '只能发一次',{ authority: 4 })
+  .action(async ({session},user)=>{
+    const muteMember=session.elements[1].attrs.id
+    const channelID=session.channelId
+    await ctx.database.upsert('mutelist',[{id:muteMember,channelID:channelID}])
+    await session.bot.muteGuildMember(channelID,muteMember,1000*60*60*0.5)
+  })
+  
 }
